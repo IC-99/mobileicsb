f132c9d5a00b4d2d193b5821f7d5021692168d70
IC-99 <ivancarlini99@gmail.com>
2021-10-15 16:21:47 +0200

    aggiunta la possibilia' di sparare

---
+++ Mobile-ICSB/Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        explode();
+    }
+
+    void explode()
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 2f);
+        Destroy(gameObject);
+    }
+
+    public void OnDestroy()
+    {
+        explode();
+    }
+
+}
--- Mobile-ICSB/Assets/Scripts/ShootJoystick.cs
+++ Mobile-ICSB/Assets/Scripts/ShootJoystick.cs
@@ -6,6 +6,13 @@ public class ShootJoystick : MonoBehaviour
 {
     public ShootJoystickIsUsing isUsing;
 
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    protected bool isShooting;
+
+    public float bulletForce = 20f;
+
     public Transform player;
     private bool touchStart = false;
 
@@ -46,14 +53,45 @@ public class ShootJoystick : MonoBehaviour
         {
             Vector2 offset = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - pointA;
             Vector2 direction = Vector2.ClampMagnitude(offset / 1500, 0.1f);
-            moveCharacter(direction);
+            rotateCharacter(direction);
+
             shootKnob.position = new Vector2(shootOuter.position.x + direction.x, shootOuter.position.y + direction.y);
         }
 
+        if (touchStart)
+        {
+            if (!isShooting)
+            {
+                StartCoroutine(shooting(0.4f));
+            }
+
+        }
+
     }
-    void moveCharacter(Vector2 direction)
+    void rotateCharacter(Vector2 direction)
     {
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
     }
+
+    void shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    public IEnumerator shooting(float time)
+    {
+        //Instantiate your projectile
+        isShooting = true;
+        yield return new WaitForSeconds(0.1f);
+        shoot();
+        //wait for some time
+        yield return new WaitForSeconds(time);
+        isShooting = false;
+        
+    }
+
+
 }
