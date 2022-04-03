c5e633fb36d329adf38940cc01c7bc989e60f3bf
Stepob <bordeianu.stefano@gmail.com>
2022-04-03 19:19:22 +0200

    OpenProjectCommit

---
+++ Mobile-ICSB/Assets/Scripts/BossEnemy.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    private bool isDying;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    private float opacity;
+
+    public GameObject healtBarUI;
+    public HealthBar healthBar;
+
+    public GameObject bulletPrefab;
+    protected bool isShooting = false;
+    public Transform player;
+    private Vector2 direzioneSparo;
+    private bool isChanging = false;
+    private int color = 0;
+    private int timer = 0;
+    public EnemyAI enAI;
+    public int spinHealth;
+    bool isSpinning = false;
+
+    public Transform firepointSingolo;
+    public Transform firepointMultiplo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentHealth = maxHealth;
+        this.healthBar.setMaxHealth(maxHealth);
+        this.isDying = false;
+        this.rb = this.GetComponent<Rigidbody2D>();
+        this.opacity = 1f;
+        this.isShooting = false;
+        InvokeRepeating("ChangeColor", 0.5f, 1f);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(this.currentHealth <= this.spinHealth && !isSpinning)
+        {
+            this.isSpinning = true;
+            this.enAI.minDistance = 0;
+            this.enAI.speed = 400;
+            this.rb.constraints = RigidbodyConstraints2D.None;
+            this.rb.AddTorque(2000);
+            this.sr.color = Color.red;
+
+        }
+
+        if (isDying)
+        {
+            this.rb.rotation += 10f;
+            this.sr.color = new Color(1f, 1f, 1f, opacity);
+            opacity -= 0.03f;
+        }
+
+        if (!isShooting && !isDying && !isChanging && !isSpinning)
+        {
+            StartCoroutine(shooting(1f));
+        }
+    }
+
+    void ChangeColor()
+    {
+        if (!isSpinning)
+        {
+            this.timer += 1;
+            if (this.timer == 3)
+            {
+                this.timer = -3;
+                this.isChanging = true;
+                if (this.color == 0) this.color = 1;
+                else this.color = 0;
+                this.animazioneCambiaColore();
+
+            }
+            if (this.timer == 0)
+            {
+                this.isChanging = false;
+            }
+        }
+    }
+
+    void animazioneCambiaColore() 
+    {
+        if (this.color == 0) sr.color = Color.blue;
+        else sr.color = Color.white;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            takeDamage(collision.collider.GetComponent<Bullet>().getDamage());
+        }
+
+        if (collision.collider.CompareTag("EnemyStella"))
+        {
+            takeDamage(collision.collider.GetComponent<EnemySella>().getDamage());
+        }
+    }
+
+    private void takeDamage(int damage)
+    {
+        this.currentHealth -= damage;
+        this.healthBar.setHealth(currentHealth);
+        if (this.currentHealth <= 0) death();
+    }
+
+    private void death()
+    {
+        this.healtBarUI.SetActive(false);
+        this.GetComponent<Collider2D>().enabled = false;
+        this.isDying = true;
+        Destroy(gameObject, 1f);
+    }
+
+
+    void shootSingolo()
+    {
+        this.direzioneSparo = new Vector2(player.position.x, player.position.y) - (Vector2) this.firepointSingolo.position;
+        this.direzioneSparo = Vector2.ClampMagnitude(this.direzioneSparo, 1f);
+        GameObject bullet = Instantiate(bulletPrefab, this.firepointSingolo.position, this.firepointSingolo.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(this.direzioneSparo * 0.5f, ForceMode2D.Impulse);
+
+        //this.ShootSound.Play();
+    }
+
+    private Vector2 rotate(Vector2 v, float delta)
+    {
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+
+    void shootMultiplo()
+    {
+        this.direzioneSparo = new Vector2(player.position.x, player.position.y) - (Vector2) this.firepointMultiplo.position;
+        this.direzioneSparo = Vector2.ClampMagnitude(this.direzioneSparo, 1f);
+        
+        GameObject bullet = Instantiate(bulletPrefab, this.firepointMultiplo.position, this.firepointMultiplo.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(this.direzioneSparo * 0.5f, ForceMode2D.Impulse);
+        
+        GameObject bullet2 = Instantiate(bulletPrefab, this.firepointMultiplo.position, this.firepointMultiplo.rotation);
+        Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+        rb2.AddForce(rotate(this.direzioneSparo,0.4f) * 0.5f, ForceMode2D.Impulse);
+        
+        GameObject bullet3 = Instantiate(bulletPrefab, this.firepointMultiplo.position, this.firepointMultiplo.rotation);
+        Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
+        rb3.AddForce(rotate(this.direzioneSparo, -0.4f) * 0.5f, ForceMode2D.Impulse);
+
+        //this.ShootSound.Play();
+    }
+
+    void shoot()
+    {
+        if (this.color == 0)
+            this.shootSingolo();
+
+        if (this.color == 1)
+            this.shootMultiplo();
+
+    }
+
+    public IEnumerator shooting(float time)
+    {
+        isShooting = true;
+        yield return new WaitForSeconds(0.1f);
+        shoot();
+        //wait for some time
+        yield return new WaitForSeconds(time);
+        isShooting = false;
+    }
+}
---
+++ Mobile-ICSB/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform GfxT;
+    public Rigidbody2D Gfx;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    public float aggroDistance;
+    public float minDistance;
+    Path path;
+    int currentWaypoint = 0;
+    //bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    public bool canRotate;
+    public float rotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path == null)
+            return;
+
+        if (this.canRotate)
+        {
+            Vector2 direzioneRotazione = new Vector2(target.position.x, target.position.y) - (Vector2)this.Gfx.position;
+            direzioneRotazione = direzioneRotazione.normalized;
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, direzioneRotazione);
+            GfxT.rotation = Quaternion.RotateTowards(GfxT.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        if (path.GetTotalLength() > aggroDistance)
+            return;
+
+        if(path.GetTotalLength() < minDistance)
+        {
+            rb.drag = 20;
+            return;
+        }
+
+
+/*        if (currentWaypoint >= path.vectorPath.Count)   
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }*/
+
+        rb.drag = 1;
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+}
